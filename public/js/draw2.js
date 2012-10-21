64529e43b613867be8f30637c76b4d60bff50fca
Frank Pinto <frank.joseph.pinto@gmail.com>
2012-10-21 15:23:25 -0400

    Added events for path creation, successful serialization of path

--- public/js/draw2.js
+++ public/js/draw2.js
@@ -1,5 +1,7 @@
-tool.fixedDistance = 10;
+//tool.fixedDistance = 10;
+var socket = io.connect('http://' + window.location.hostname);
 
+var thickness = new Point({length: 5, angle: null});
 var path;
 var strokeEnds = 2;
 
@@ -16,16 +18,17 @@ function onMouseDrag(event) {
         addStrokes(event.middlePoint, event.delta * -1);
     } else {
         var step = event.delta / 2;
-        step.angle += 90;
+        thickness.angle = step.angle + 90;
 
-        var top = event.middlePoint + step;
-        var bottom = event.middlePoint - step;
+        var top = event.middlePoint + thickness;
+        var bottom = event.middlePoint - thickness;
 
         path.add(top);
         path.insert(0, bottom);
     }
     
     lastPoint = event.middlePoint;
+    socket.emit('mousedrag', {point: event.point});
 }
 
 function onMouseUp(event) {
@@ -33,6 +36,8 @@ function onMouseUp(event) {
     delta.length = tool.maxDistance;
     addStrokes(event.point, delta);
     path.closed = true;
+    var segments = serializeSegments(path.segments);
+    socket.emit('mouseup', {segments: segments});
 }
 
 function addStrokes(point, delta) {
@@ -50,3 +55,17 @@ function addStrokes(point, delta) {
         path.insert(0, strokePoint);
     }
 }
+
+function serializeSegments(segments)
+{
+    for (i = 0; i < segments.length; i++)
+    {
+      segment = segments[i];
+      delete segment._path;
+      delete segment._handleIn._owner;
+      delete segment._handleOut._owner;
+      delete segment._point._owner;
+    }
+    return segments;
+}
+

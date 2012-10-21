ecc290ec8aae0ec93556cc6245325918af0dc99a
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2012-10-21 13:41:57 -0400

    Revert "Changing line thickness"
    This reverts commit a82ac606116cfd67e5343d292552e006ce900ed6.

--- public/js/draw.js
+++ public/js/draw.js
@@ -1,42 +1,99 @@
-var thickness = 7;
-var greytone = 0.9;
+////////////////////////////////////////////////////////////////////////////////
+// This script belongs to the following tutorial:
+//
+// http://scriptographer.org/tutorials/geometry/working-with-mouse-vectors/#adding-brush-stroke-ends
 
-var function greyset(tone) {
-    greytone = tone;
-}
-
-
-
-
-
-var color = new RgbColor(0, 0, 0, greytone); 
-//change last number between 0=white and 1=black
-
-tool.minDistance = thickness;
-tool.maxDistance = thickness*3;
+tool.fixedDistance = 80;
 
 var path;
+var strokeEnds = 0.5;
+var dataForServer;
 
-function onMouseDown(event) {
-    path = new Path();
-    path.fillColor = color;
-    path.add(event.point);
+function prevent(e) {
+  e.preventDefault();
 }
 
+// make instance of path and setup path attributes
+function onMouseDown(event) {
+  prevent(event);
+  dataForServer = {
+    type: 'mousedown'
+  }
+  socket.emit('canvas change', dataForServer);
+  path = new Path();
+  path.fillColor = 'black';
+}
+
+var lastPoint;
+
 function onMouseDrag(event) {
+  // If this is the first drag event, add the strokes at the start:
+  if (event.count == 1) {
+    addStrokes(event.middlePoint, event.delta * -1);
+    dataForServer = {
+      middlePoint: event.middlePoint,
+      delta: event.delta,
+      count: event.count,
+      type: 'mousedrag'
+    }
+    socket.emit('canvas change', dataForServer);
+  } else {
     var step = event.delta / 2;
     step.angle += 90;
-    
-    var top = event.middlePoint + step*0.6;
-    var bottom = event.middlePoint - step*0.6;
-    
+
+    // The top point: the middle point + the step rotated by 90 degrees:
+    var top = event.middlePoint + 10;//step;
+
+    // The bottom point: the middle point - the step rotated by 90 degrees:
+    var bottom = event.middlePoint - 10;//step;
+
+    dataForServer = {
+      step: step,
+      lastPoint: lastPoint,
+      topPoint: top,
+      bottomPoint: bottom,
+      count: event.count,
+      type: 'mousedrag'
+    }
+    socket.emit('canvas change', dataForServer);
+
     path.add(top);
     path.insert(0, bottom);
-    path.smooth();
+
+  }
+  path.smooth();
+  lastPoint = event.middlePoint;
 }
 
 function onMouseUp(event) {
-    path.add(event.point);
-    path.closed = true;
-    path.smooth();
+  var delta = event.point - lastPoint;
+  delta.length = tool.maxDistance;
+  addStrokes(event.point, delta);
+
+  dataForServer = {
+    lastPoint: lastPoint,
+    eventPoint: event.point,
+    delta: delta,
+    type: 'mouseup'
+  }
+  socket.emit('canvas change', dataForServer);
+
+  path.closed = true;
+  path.smooth();
+}
+
+function addStrokes(point, delta) {
+  var step = delta;
+  var strokePoints = strokeEnds * 2 + 1;
+  point -= step / 2;
+  step /= strokePoints - 1;
+  for (var i = 0; i < strokePoints; i++) {
+    var strokePoint = point + step * i;
+    var offset = delta * (Math.random() * 0.3 + 0.1);
+    if (i % 2) {
+      offset *= -1;
+    }
+    strokePoint += offset;
+    path.insert(0, strokePoint);
+  }
 }

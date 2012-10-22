f7042d290fd69400874ded7213b5d939ac79a51e
Frank Pinto <frank@onswipe.com>
2012-10-22 03:49:08 +0000

    [minor] twitterauth stuff, but something weird is happening with express.session()

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 #!/usr/local/bin/node
+/*var OAuth= require('oauth').OAuth;
 
+var oauth = new OAuth(
+  'https://api.twitter.com/oauth/request_token',
+  'https://api.twitter.com/oauth/access_token',
+  'UFzttyoZssfh6V8o5uapA',
+  'HI5CLqlwQtuyPOnp9j1uW515kpe5wzN7D8JLjGH3Yw',
+  '1.0',
+  'http://frankpinto.info/auth/twitter/callback',
+  'HMAC-SHA1'
+);
+*/
 var express = require('express');
-var app     = express.createServer();
-var io      = require('socket.io').listen(app, {log: false});
+var app = express.createServer();
+var io = require('socket.io').listen(app, {log: false});
 
 io.sockets.on('connection', function (socket) {
   console.log('Drawer named ' + socket.id + ' has joined the session.');
@@ -16,15 +27,49 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-app.use(express.cookieParser());
-//app.use(express.session({secret: 'pandas'}));
-app.use(express.static(__dirname + '/public'));
-
+app.configure(function () {
+  app.use(express.favicon());
+  app.use(express.cookieParser());
+  //app.use(express.session({secret: 'pandas'}));
+  app.use(express.static(__dirname + '/public'));
+});
 app.get('/', function(req, res, next) {
   res.render('index.ejs', {layout: false});
   res.end();
 });
 
+/*app.get('/auth/twitter', function (req, res) {
+  console.log('got called');
+  oauth.getOAuthRequestToken( function (err, oauth_token, oauth_token_secret, results) {
+    if (err) {
+      throw err;
+    } else {
+      req.session.oauth = {};
+      req.session.oauth.token = oauth_token;
+      req.session.oauth.token_secret = oauth_token_secret;
+      res.redirect('https://twitter.com/oauth/authenticate?oauth_token=' + oauth_token);
+    }
+  });  
+});*/
+/*
+app.get('/auth/twitter/callback', function (req, res) {
+  if (req.session.oauth) {
+    req.session.oauth.verifier = req.query.oauth_verifier;
+    var oa = req.session.oauth;
+
+    oauth.getOAuthAccessToken(oa.token, oa.token_secret, oa.verifier,
+      function (err, oauth_access_token, oauth_access_token_secret, results) {
+        if (err) {
+          throw err;
+        } else {
+          req.session.oauth.access_token = oauth_access_token;
+          req.session.oauth.access_token_secret = oauth_access_token_secret;
+          res.redirect('/');
+        }
+    });
+  }
+});
+*/
 app.listen(3000, function() {
     console.log('Now listening on port 3000');    
 });
